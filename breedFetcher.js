0a9466346bf2b7c40c5e15a476ed1b29f67593cc
Zongxi Li <zongxiliwork@gmail.com>
2019-08-29 21:04:33 +0000

    Callback added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,27 @@
-const catName = process.argv.slice(2);
-let URL = "https://api.thecadsfatapi.com/v1/breeds/search?q=";
-URL += catName;
-
-const request = require('request');
-request.get(URL, (error, response, body) => {
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  if (error !== null) {
-    // Address not found
-    console.log("Address not found.");
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      // Breed Not Found cases
-      console.log("Returning nothing.");
+const fetchBreedDescription = function(breedName, callback) {
+  let URL = "https://api.thecatapi.com/v1/breeds/search?q=";
+  URL += breedName;
+  const request = require('request');
+  request.get(URL, (error, response, body) => {
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (error !== null) {
+      // Address not found
+      // console.log("Address not found.");
+      callback("Address nothing.", null);
     } else {
-      console.log(data[0]["description"]);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        // Breed Not Found cases
+        // console.log("Returning nothing.");
+        callback("Returning nothing.", null);
+      }
+      // The demand input
+      else {
+        // console.log(data[0]["description"]);
+        callback(null, data[0]["description"]);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
